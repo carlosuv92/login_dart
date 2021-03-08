6c861e9699463fbbb6298069bf4d6feb25b675c9
Felipe Vega <felipe.vega@amgsoft.us>
2021-03-08 17:07:48 -0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/devcredit/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.devcredit
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:devcredit/Screens/Welcome/welcome_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:devcredit/contains.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Login',
+      theme: ThemeData(
+        primaryColor: kPrimaryColor,
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      home: WelcomeScreen(),
+    );
+  }
+}
